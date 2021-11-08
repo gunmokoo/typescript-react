2644b80e4a5ccbee2744772bfc51ae5f846954e6
Gunmo Koo <minju1348@gmail.com>
2021-11-08 18:00:35 +0900

    styled components

--- src/Number.tsx
+++ src/Number.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.span``;
+interface IContainerProps {
+  isBlue: boolean;
+}
+
+const Container = styled.span<IContainerProps>`
+  color: ${(props) => (props.isBlue ? "blue" : "black")};
+`;
 
 interface IProps {
   count: number;
 }
 
 const Number: React.FunctionComponent<IProps> = ({ count }) => (
-  <Container>{count}</Container>
+  <Container isBlue={count > 10}>{count}</Container>
 );
 
 export default Number;
